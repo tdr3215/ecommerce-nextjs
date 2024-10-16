8bae277c002003ef12365189369beb04a25b7408
Tiffany Rose <tiffany.drose@gmail.com>
2024-10-16 10:42:34 -0400

    fixed zod errors for vercel build

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,8 +4,8 @@ const envSchema = zod.object({
     DATABASE_URL: zod.string().min(1),
     GOOGLE_CLIENT_ID: zod.string().min(1),
     GOOGLE_CLIENT_SECRET: zod.string().min(1),
-    NEXTAUTH_URL: zod.string(),
-    NEXTAUTH_SECRET: zod.string(),
+    NEXTAUTH_URL: zod.string().optional(),
+    NEXTAUTH_SECRET: zod.string().optional(),
 })
 
 export const env = envSchema.parse(process.env);
